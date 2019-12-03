f3eb25eb4eb36b38f6a018d238664a79f99334a9
jemarjames222 <jemarjamesj@gmail.com>
2019-12-03 14:42:27 +0800

    model view controller

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,13 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        	<h1>Student list here</h1>
-            <a class="btn btn-primary" href="{{ route('students.add') }}">Add Student</a>
+        	<center><h1>WELCOME TO STUDENT LIST OF CEC</h1></center><br>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add-student-modal">Add Student</button>
+            @include('students.modals._add-student-modal')
         	<table class="table">
         		<thead>
         			<tr>
         				<th>#</th>
         				<th>Name</th>
+                        <th>Action</th>
         			</tr>
         		</thead>
         		<tbody>
@@ -18,6 +20,10 @@
         			<tr>
         				<td>{{ $loop->iteration }}</td>
         				<td>{{ $student->first_name }} {{ $student->middle_name }} {{ $student->last_name }}</td>
+                        <td>
+                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit-student-modal-{{$student->id}}">Edit</button>
+                            @include('students.modals._edit-student-modal')
+                        </td>    
         			 @endforeach
                     </tr>
         		</tbody>
